8d6ff6b8be04dddc8eb7145c4dffe05e72f358b2
Prathama-Singh <spratima22@gmail.com>
2026-03-11 00:04:59 +0530

    Implement maxArea method for ContainerWithMostWater

--- patterns/twoPointers/ContainerWithMostWater.java
+++ patterns/twoPointers/ContainerWithMostWater.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maxArea(int[] height) {
+        int len = height.length;
+        int maxArea = 0;
+        int l = 0, r = len-1;
+        while(l < r){
+            int area = Math.min(height[r],height[l]) * (r - l);
+            maxArea = Math.max(maxArea, area);
+            if(height[r] < height[l])
+                r--;
+            else l++;
+        }
+        return maxArea;
+    }
+}
