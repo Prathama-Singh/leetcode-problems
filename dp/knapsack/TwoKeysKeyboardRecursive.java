61f4a419250852e70a526d585db25dfd75270e16
Prathama Singh <spratima22@gmail.com>
2026-07-01 21:09:14 +0530

    Implement paste operation in TwoKeysKeyboardRecursive
    Added logic for paste operation in recursive helper.

--- dp/knapsack/TwoKeysKeyboardRecursive.java
+++ dp/knapsack/TwoKeysKeyboardRecursive.java
@@ -10,8 +10,10 @@ class Solution {
             return 0;
         if(count > n )
             return 1000;
-      
+        //paste      
         int res1 = 1 + helper(count + paste, paste, n);
+        
+        //copy + paste
         int res2 = 2 + helper (count + count, count, n);
         return Math.min (res1, res2);
     }
