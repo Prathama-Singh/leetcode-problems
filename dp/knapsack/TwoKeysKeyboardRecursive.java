52472e4b451414b86e525ba7918d64c788bb5f9d
Prathama Singh <spratima22@gmail.com>
2026-07-01 21:05:16 +0530

    Implement recursive solution for minSteps problem

---
+++ dp/knapsack/TwoKeysKeyboardRecursive.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSteps(int n) {
+        if(n == 1) return 0;
+        int count = 1, paste = 1;
+        return (1 + helper(1, 1, n));
+    }
+
+    int helper(int count, int paste, int n){
+        if(count == n)
+            return 0;
+        if(count > n )
+            return 1000;
+      
+        int res1 = 1 + helper(count + paste, paste, n);
+        int res2 = 2 + helper (count + count, count, n);
+        return Math.min (res1, res2);
+    }
+    
+}
