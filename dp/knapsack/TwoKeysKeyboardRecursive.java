ebbaf2968d1612be7195fc33bf9fb7400e8b33e5
Prathama Singh <spratima22@gmail.com>
2026-07-01 21:46:09 +0530

    Refactor helper method parameters and logic

--- dp/knapsack/TwoKeysKeyboardRecursive.java
+++ dp/knapsack/TwoKeysKeyboardRecursive.java
@@ -1,21 +1,21 @@
 class Solution {
     public int minSteps(int n) {
         if(n == 1) return 0;
-        int count = 1, paste = 1;
         return (1 + helper(1, 1, n));
     }
 
-    int helper(int count, int paste, int n){
+    int helper(int count, int clipboard, int n){
         if(count == n)
             return 0;
         if(count > n )
             return 1000;
+
         //paste      
-        int res1 = 1 + helper(count + paste, paste, n);
+        int paste = 1 + helper(count + clipboard, clipboard, n);
         
         //copy + paste
-        int res2 = 2 + helper (count + count, count, n);
-        return Math.min (res1, res2);
+        int copyallPaste = 2 + helper (count + count, count, n);
+        return Math.min (paste, copyallPaste);
     }
     
 }
